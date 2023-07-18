7b72bd503adc49158890d5ec01971e96fb80f0e9
yeahzxnn <yejin0621@tukorea.ac.kr>
2023-07-18 17:22:54 +0900

    AppleLoginSample

---
+++ AppleLoginPractice/AppleLoginPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleLoginPracticeApp.swift
+//  AppleLoginPractice
+//
+//  Created by 신예진 on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct AppleLoginPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
